d7270ef8de2d85cc3403c3599d6c5e071f0d2c31
waspryo <wasp2123321@gmail.com>
2019-12-03 22:46:54 +0900

    get the data from jsonplaceholder

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div id="app">
-    
+    <h1 v-for="(item, index) in posts" :key="index">{{ item.body }}</h1>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
-  name: 'app',
+  name: "app",
   mounted() {
+    // activateするメソッドmounted
     // Storeのアクションメソッドを呼び出す
-    this.$store.dispatch('loadPosts')
+    this.$store.dispatch("loadPosts");
+  },
+  computed: {
+    ...mapState([
+      // storeのpostsメソッドから引っ張ってきたもの
+      "posts"
+    ])
   }
-}
+};
 </script>
