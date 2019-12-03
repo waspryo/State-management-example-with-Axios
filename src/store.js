61f67027b3b2716e663df11b2c9c600399cb0a61
waspryo <wasp2123321@gmail.com>
2019-12-03 22:25:30 +0900

    define actions and mutations

--- src/store.js
+++ src/store.js
@@ -10,16 +10,22 @@ export default new vuex.Store({
   },
   actions: {
     //   親のApp.vueコンポーネントに呼び出してもらう
-    loadPosts () {
+    loadPosts ({commit}) {
         axios.get('https://jsonplaceholder.typicode.com/posts')
         .then(res => {
             console.log(res.data)
+            let posts = res.data
+            commit('SET_POSTS', posts)
         }).catch(error => {
             console.log(error)
         })
     }
+    // axtionsにmutationsにコミットするデータを定義し
+    // mutationsにはstateのパラメーターを渡し新しく28nのようにapiのデータを使う
   },
   mutations: {
-
+    SET_POSTS (state, posts) {
+        state.posts = posts
+    }
   }
 });
